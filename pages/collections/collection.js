42d05e64b3ae11d45fdb8319f6c302a0bb4f618d
Ian Mucheru <imucheru.im@gmail.com>
2023-10-05 13:21:17 +0300

    Updated Featured Collections Page

--- pages/collections/collection.js
+++ pages/collections/collection.js
@@ -4,3 +4,52 @@
  */
 
 "use strict";
+
+/*** Imports ***/
+import { client } from "../../js/api_configure.js";
+import { collectionCard } from "../../js/collection_card.js";
+
+/*** Render Featured Collections ***/
+const $collectionGrid = document.querySelector("[data-collection-grid]");
+const perPage = 36;
+let currentPage = 1;
+let totalPage = 0;
+
+/***
+ * @param {Number} page Page Number
+ */
+
+const loadCollections = function (page) {
+  client.collections.featured({ per_page: perPage, page: page }, (data) => {
+    totalPage = Math.ceil(data.total_results / perPage);
+
+    data.collections.forEach((collection) => {
+      const $collectionCard = collectionCard(collection);
+
+      $collectionGrid.appendChild($collectionCard);
+    });
+
+    isLoaded = true;
+    currentPage >= totalPage && ($loader.style.display = "none");
+  });
+};
+
+loadCollections(currentPage);
+
+/*** Load More Collections ***/
+const $loader = document.querySelector("[data-loader]");
+let isLoaded = false;
+
+const loadMore = function () {
+  if (
+    $loader.getBoundingClientRect().top < window.innerHeight * 2 &&
+    currentPage <= totalPage &&
+    isLoaded
+  ) {
+    currentPage++;
+    loadCollections(currentPage);
+    isLoaded = false;
+  }
+};
+
+window.addEventListener("scroll", loadMore);
